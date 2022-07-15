dafdf346f4005cd16bf44a55c729b7115491fb6a
Sarushili <38129719+sarushili0430@users.noreply.github.com>
2022-07-15 17:17:58 +0900

    Update mainfunc.ino

--- mainfunc.ino
+++ mainfunc.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
-#define MAX_CHAR 1104
+#define MAX_CHAR 256
 
 rgb_lcd lcd;
 
@@ -11,51 +11,9 @@ void setup() {
   lcd.setRGB(0, 255, 255);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  String value;
-  char store[MAX_CHAR];
-  char shifted[MAX_CHAR];
-  int *string;
-  int shift;
-  while(Serial.available()==0){
-    shift = (analogRead(A0)-511)/38;
-    Serial.println(shift);
-    //lcd.clear();
-    //lcd.setCursor(0,0);
-    //lcd.print(shift);
-    delay(100);
-  }
-  lcd.clear();
-  value = Serial.readString();
-  value.toUpperCase();
-  value.toCharArray(store,MAX_CHAR); //String -> char
-  string = toASCII(store);
-  for(int i=0;i<=strlen(store)-2;i++){
-    string[i] += shift; //Shifts the letter
-    if (string[i]>90){
-      string[i] -= 25; //if 'Z' goes to 'A'
-    }
-    if(string[i] == 32+shift){
-      string[i] -= shift;  
-    } else if(string[i]<65){
-      string[i] += 26;
-    }
-    shifted[i] = string[i];
-    Serial.print(shifted[i]);
-    if(i<16){
-      lcd.setCursor(i,0);
-    } else{
-      lcd.setCursor(i-16,1);
-    }
-    lcd.print(shifted[i]);
-  }
-  Serial.print(store);
-}
-
 //Converting alphabets to ASCII
-int *toASCII(char *string){
-  //Serial.print(string);
+int *toASCII(char string[]){
+  //Serial.println(string);
   int Size = strlen(string);
   int *Array = (int*)malloc(sizeof(int)*(Size+10));
   for(int i=0; i<=Size;i++){
@@ -68,4 +26,63 @@ int *toASCII(char *string){
     //Serial.println(Array[i]);
   }
   return Array;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String value;
+  char store[MAX_CHAR];
+  char shifted[MAX_CHAR];
+  int *string;
+  int shift;
+  int page;
+  int previous;
+  while(Serial.available()==0){
+    //shift = (analogRead(A0)-511)/38;
+    shift = 0;
+    Serial.println(shift);
+    //lcd.clear();
+    //lcd.setCursor(0,0);
+    //lcd.print(shift);
+    delay(100);
+  }
+  lcd.clear();
+  value = Serial.readString();
+  value.toUpperCase();
+  value.toCharArray(store,MAX_CHAR); //String -> char
+  Serial.println(store);
+  string = toASCII(store);
+  for(int i=0;i<=strlen(store)-2 ;i++){
+    string[i] += shift; //Shifts the letter
+    if (string[i]>90){
+      string[i] -= 25; //if 'Z' goes to 'A'
+    }
+    if(string[i] == 32+shift){
+      string[i] -= shift;  
+    } else if(string[i]<65){
+      string[i] += 26;
+    }
+    shifted[i] = string[i];
+    //Serial.print(shifted[i]);
+    if(i<16){
+      lcd.setCursor(i,0);
+    } else{
+      lcd.setCursor(i-16,1);
+    }
+    lcd.print(shifted[i]);
+  }
+  while(Serial.available()==0){
+    page = analogRead(A0)/515;
+    if(page != previous){ lcd.clear();}
+    for(int i=0; i<=31 && i+page*32 <= strlen(store)-2; i++){
+        if(i<16){
+          lcd.setCursor(i,0);
+        } else{
+          lcd.setCursor(i-16,1);
+        }
+        lcd.print(shifted[i+page*32]);
+      }
+    previous = page;
+  }
+  free(string);
 }
